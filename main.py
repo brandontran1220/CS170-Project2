1aa1defa654e880e65106cae93baff151d17ac89
brandontran1220 <brandontran1220@gmail.com>
2026-03-13 22:46:03 -0700

    Add numba and numpy to optimize runtime

--- main.py
+++ main.py
@@ -1,28 +1,21 @@
 import time
+import numpy as np
+from numba import njit
 
 def load_dataset(filename):
-    features = []
-    class_name = []
-
-    with open(filename, 'r') as file: ## open the file for reading
-        for line in file:
-            line = line.strip()
-            if not line:
-                continue
-            
-            nums = [float(x) for x in line.split()] ## split the line into numbers and convert them to float
-
-            class_name.append(nums[0])  ## the class name is the first number in the row
-            features.append(nums[1:])  ## the rest of the numbers are the features
-    
+    data = np.loadtxt(filename)
+    class_name = data[:, 0]
+    features = data[:, 1:]
     return class_name, features
 
+@njit
 def euclidean_distance(first, second, feature_set):
     distance = 0.0
     for i in feature_set: ## iterate through the specified features
         distance += (first[i - 1] - second[i - 1]) ** 2 ## calculate the squared difference for each feature and sum them up
     return distance ** 0.5 ## return the Euclidean distance
 
+@njit
 def leave_one_out(features, class_name, feature_set):
     number_correctly_classified = 0
 
@@ -114,7 +107,7 @@ def backward_elimination(features, class_name, cnt_features):
     return best_features, best_accuracy
 
 def main():
-    print("Welcome to Brandon's Feature Selection Algorithm")
+    print("Welcome to Brandon Tran's Feature Selection Algorithm")
     file_name = input("Type in the name of the file to test: ").strip()
     class_name, features = load_dataset(file_name) ## load the dataset from the specified file
 
